fe99714006c5b84272163cdb252ab42cb3390034
yashk21 <yashk8119@gmail.com>
2024-04-12 14:11:58 +0530

    Express-Session Added

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 dotenv.config({
   path: "./.env",
 });
@@ -18,6 +19,12 @@ app.use(
     limit: "20kb",
   })
 );
+app.use(session({
+  secret: process.env.EXPRESS_SESSION_SECRET, // Replace 'your-secret-key' with a secret string
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // Set secure to true if using HTTPS
+ }));
 // app.use(express.urlencoded({ extend: true, limit: "16kb" }));
 
 import router from "./routes/user.routes.js";
