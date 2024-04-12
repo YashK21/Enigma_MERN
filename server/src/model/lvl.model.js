b654b55cf51be43d4cbbcbd6a4280b558f7316dd
yashk21 <yashk8119@gmail.com>
2024-04-12 14:07:32 +0530

    Lvl_Ans Field Added

--- server/src/model/lvl.model.js
+++ server/src/model/lvl.model.js
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
 
 const lvlSchema = new mongoose.Schema({
-    Lvl_no: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    Lvl_Img:{
-        type:String,
-        required: true,
-        unique: true,
-    }
-})
-export const Lvl = mongoose.model("Lvl",lvlSchema )
+  Lvl_no: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  Lvl_Img: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  Lvl_Ans: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+export const Lvl = mongoose.model("Lvl", lvlSchema);
