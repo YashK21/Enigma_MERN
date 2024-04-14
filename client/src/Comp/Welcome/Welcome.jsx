b26ca51667cddafff090edbbf49a4a485f2fe186
yashk21 <yashk8119@gmail.com>
2024-04-14 20:24:41 +0530

    Styling Updated

--- client/src/Comp/Welcome/Welcome.jsx
+++ client/src/Comp/Welcome/Welcome.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import bg from "../../bgimg/bg.jpg";
 const Welcome = () => {
   const navigate = useNavigate();
   const handleLoginAndSignUp = (e) => {
@@ -7,11 +8,20 @@ const Welcome = () => {
     else if (e.target.id === "signup") navigate("/signup");
   };
   return (
-    <div>
+    <div
+      className="flex flex-col items-center justify-center h-screen text-white"
+      style={{
+        backgroundImage: `url(${bg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <h1 className="text-3xl font-bold mb-8 text-blue-300">Welcome to the land of mystery!</h1>
       <p
         id="signup"
         onClick={handleLoginAndSignUp}
-        style={{ cursor: "pointer" }}
+        // style={{ cursor: "pointer" }}
+        className="bg-gray-800 hover:bg-gray-700 cursor-pointer text-purple-400 font-semibold py-3 px-8 rounded-full focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50 transition duration-300 shadow-lg"
       >
         SignUp
       </p>
@@ -19,7 +29,8 @@ const Welcome = () => {
       <p
         id="login"
         onClick={handleLoginAndSignUp}
-        style={{ cursor: "pointer" }}
+        // style={{ cursor: "pointer" }}
+        className="bg-gray-800 hover:bg-gray-700 cursor-pointer text-purple-400 font-semibold py-3 px-8 rounded-full focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50 transition duration-300 shadow-lg"
       >
         Login
       </p>
