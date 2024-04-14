a540e733a2b65c2d6590e0dcdc9fca2c94fce88a
yashk21 <yashk8119@gmail.com>
2024-04-14 20:31:07 +0530

    Logout Route for Admin added with verifyAdminJWT

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import admin from "../admin/admin.controller.js";
-import adminLogin from "../admin/admin.login.js";
+import { adminLogin } from "../admin/admin.login.js";
+import { adminLogout } from "../admin/admin.login.js";
+import { verifyAdminJWT } from "../middlewares/auth.middleware.js";
 const adminRouter = Router();
 adminRouter.route("/admin/login").post(adminLogin);
 adminRouter.route("/admin").post(admin);
+adminRouter.route("/admin/logout").post(verifyAdminJWT, adminLogout);
 
 export default adminRouter;
