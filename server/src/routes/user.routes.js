39e0ac9a29e31d9d8480a3b5a8cca9149ea81e91
yashk21 <yashk8119@gmail.com>
2024-04-14 20:31:43 +0530

    Admin and User Route Seperated

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -6,17 +6,12 @@ import {
   logoutUser,
   registerUser,
 } from "../controllers/user.controller.js";
-import verifyJWT from "../middlewares/auth.middleware.js";
-import admin from "../admin/admin.controller.js";
-import adminLogin from "../admin/admin.login.js";
+import {verifyJWT} from "../middlewares/auth.middleware.js";
 const router = Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/level/:lvl").get(verifyJWT,level);
 router.route("/levelanscheck/:lvl").post(verifyJWT,levelAnsCheck);
 router.route("/logout").post(verifyJWT, logoutUser);
-//admin route
-router.route("/admin/login").post(adminLogin)
-router.route("/admin").post(admin);
 
 export default router;
