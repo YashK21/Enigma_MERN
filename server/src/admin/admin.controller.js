b3a7732db98de9d2391eadb4760e7ab19f3e46ec
yashk21 <yashk8119@gmail.com>
2024-04-14 21:13:21 +0530

    File Structure Changed

--- server/src/admin/admin.controller.js
+++ server/src/admin/admin.controller.js
@@ -1,44 +1,79 @@
-import { Lvl } from "../model/lvl.model.js";
+import { Admin } from "../model/admin.model.js";
+import ApiError from "../utils/ApiError.js";
 import ApiRes from "../utils/ApiRes.js";
-import ApiError from "../utils/ApiRes.js";
-
-const admin = async (req, res) => {
-
-  //for uploading Lvls
-  let { Lvl_No ,Lvl_Ans } = req.body;
-  const file = req.files.Lvl_Img;
-  // console.log(file);
-  if (!file) {
-    return res.status(400).json(new ApiError(400, "No file uploaded"));
-  }
-  const base64Img = Buffer.from(file.data).toString("base64");
-  if (
-    [Lvl_No, base64Img, Lvl_Ans].some(
-      (emptyField) => String(emptyField).trim() === ""
-    )
-  ) {
-    return res.status(400).json(new ApiError(400, "All Fields are required"));
-  }
-  const lvlExisted = await Lvl.findOne({
-    $or: [{ Lvl_No }, { Lvl_Img: base64Img }, { Lvl_Ans }],
-  });
-  if (lvlExisted) {
-    return res.status(400).json(new ApiError(400, "Level Already Exists"));
-  }
-  const levelCreated = await Lvl.create({
-    Lvl_No,
-    Lvl_Img: base64Img,
-    Lvl_Ans 
-  });
-
-  const levelSaved = await Lvl.findOne(levelCreated._id).select("-Lvl_Ans");
-  if (!levelSaved) {
+const genAdminAccessToken = async (adminId) => {
+  try {
+    const admin = await Admin.findById(adminId);
+    const adminAccessToken = await admin.genAdminToken();
+    admin.adminAccessToken = adminAccessToken;
+    await admin.save();
+    return { adminAccessToken };
+  } catch (error) {
     return res
       .status(500)
-      .json(new ApiError(500, "Something went wrong while adding level"));
+      .json(
+        new ApiError(500, "Something went wrong while generating tokens", error)
+      );
   }
+};
+const adminLogin = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username) {
+    // throw new ApiError(400, "username is required");
+    return res.status(400).json(new ApiError(400, "username is required"));
+  }
+  if (!password) {
+    return res.status(400).json(new ApiError(400, "password is required"));
+  }
+
+  const admin = await Admin.findOne({
+    username,
+  });
+  if (!admin) {
+    // throw new ApiError(404, "User not found");
+    return res.status(400).json(new ApiError(404, "Admin not found"));
+  }
+  const passwordValid = await admin.passCheck(password);
+  if (!passwordValid) {
+    // throw new ApiError(401, "Password is wrong!!");
+    return res.status(401).json(new ApiError(404, "Password is wrong!!"));
+  }
+
+  const { adminAccessToken } = await genAdminAccessToken(admin._id);
+  const loggedInAdmin = await Admin.findById(admin._id).select(
+    "-password -adminAccessToken"
+  );
+  // console.log(loggedInAdmin)
   return res
     .status(200)
-    .json(new ApiRes(200, Lvl_No, `Level No: ${Lvl_No} Added Successfully`));
+    .cookie("adminAccessToken", adminAccessToken)
+    .json(
+      new ApiRes(
+        200,
+        {
+          admin: loggedInAdmin,
+          adminAccessToken,
+        },
+        "Admin logged in successfully"
+      )
+    );
 };
-export default admin;
+const adminLogout = async (req, res) => {
+  await Admin.findByIdAndUpdate(
+    req.admin._id,
+    {
+      $unset: {
+        adminAccessToken: 1,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+  return res
+    .status(200)
+    .clearCookie("connect.sid")
+    .clearCookie("adminAccessToken")
+    .json(new ApiRes(200, {}, "Admin LoggedOut SuccessFully!"));
+};
+export { adminLogin, adminLogout };
