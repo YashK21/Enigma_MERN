5d64974f4a6c96b3ea1335d2c06773b6b17fefd7
yashk21 <yashk8119@gmail.com>
2024-04-14 21:12:13 +0530

    File Structure Changed

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import admin from "../admin/admin.controller.js";
-import { adminLogin } from "../admin/admin.login.js";
-import { adminLogout } from "../admin/admin.login.js";
+import adminUpload from "../admin/admin.lvl.upload.js";
+import { adminLogin } from "../admin/admin.controller.js";
+import { adminLogout } from "../admin/admin.controller.js";
 import { verifyAdminJWT } from "../middlewares/auth.middleware.js";
 const adminRouter = Router();
 adminRouter.route("/admin/login").post(adminLogin);
-adminRouter.route("/admin").post(admin);
+adminRouter.route("/admin").post(adminUpload);
 adminRouter.route("/admin/logout").post(verifyAdminJWT, adminLogout);
 
 export default adminRouter;
