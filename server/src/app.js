6165516b281bce195d6503e9a77a185e4d38d1eb
yashk21 <yashk8119@gmail.com>
2024-04-18 23:52:22 +0530

    updated

--- server/src/app.js
+++ server/src/app.js
@@ -9,12 +9,13 @@ dotenv.config({
 });
 const app = express();
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 app.use(
   express.json({
     limit: "20kb",
